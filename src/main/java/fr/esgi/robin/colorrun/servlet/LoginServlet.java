dcb9462ba5e5b8ddbea97f8da55aafb78b57b60e
RobinCouture <147134048+RobinCouture@users.noreply.github.com>
2025-05-14 12:50:44 +0200

    Create LoginServlet.java

---
+++ src/main/java/fr/esgi/robin/colorrun/servlet/LoginServlet.java
@@ -0,0 +1,40 @@
+package fr.esgi.robin.colorrun.servlet;
+
+import fr.esgi.robin.colorrun.business.Utilisateur;
+import fr.esgi.robin.colorrun.repository.UtilisateurRepository;
+import fr.esgi.robin.colorrun.repository.impl.UtilisateurRepositoryImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private final UtilisateurRepository utilisateurRepository = new UtilisateurRepositoryImpl();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String motDePasse = req.getParameter("motDePasse");
+
+        Utilisateur user = utilisateurRepository.findByEmail(email);
+
+        if (user != null && user.getMotDePasse().equals(motDePasse)) {
+            HttpSession session = req.getSession();
+            session.setAttribute("user", user);
+            resp.sendRedirect("/");
+        } else {
+            req.setAttribute("errorMessage", "Email ou mdp incorrect");
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+    }
+}
